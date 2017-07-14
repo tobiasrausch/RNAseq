a89672eeaf39d6ed0afb15874ac67042ec958868
tobiasrausch <rauschtobi@gmail.com>
2017-07-14 15:45:05 +0200

    geuvadis

--- example/gEUVADIS.sh
+++ example/gEUVADIS.sh
@@ -2,23 +2,6 @@
 
 export PATH=/g/funcgen/bin:${PATH}
 
-if [ $# -ne 1 ]
-then
-    echo "**********************************************************************"
-    echo "RNA-Seq processing pipeline."
-    echo "This program comes with ABSOLUTELY NO WARRANTY."
-    echo ""
-    echo "Contact: Tobias Rausch (rausch@embl.de)"
-    echo "**********************************************************************"
-    echo ""
-    echo "Usage: $0 <hg19.fa>"
-    echo ""
-    exit -1
-fi
-    
-
-HG=${1}
-
 # Download HG00096 FASTQ files
 wget "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR188/ERR188040/ERR188040_1.fastq.gz"
 wget "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR188/ERR188040/ERR188040_2.fastq.gz"
