3ad7b59174cfb391582b745d011db7195324dc7c
tobiasrausch <rauschtobi@gmail.com>
2018-06-21 11:21:04 +0200

    fixed example

--- example/gEUVADIS.sh
+++ example/gEUVADIS.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export PATH=/g/funcgen/bin:${PATH}
-
 # Download HG00096 FASTQ files
 wget "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR188/ERR188040/ERR188040_1.fastq.gz"
 wget "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR188/ERR188040/ERR188040_2.fastq.gz"
