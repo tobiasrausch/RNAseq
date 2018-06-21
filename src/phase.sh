87ccde2d05f8dd0f818dc64332c648096354bea6
tobiasrausch <rauschtobi@gmail.com>
2018-06-21 13:04:45 +0200

    all autosomes

--- src/phase.sh
+++ src/phase.sh
@@ -26,8 +26,7 @@ bcftools index ${OP}.input.bcf
 
 # Phase against panel
 FILES=""
-#for CHR in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
-for CHR in 1 2
+for CHR in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
 do
     echo "Eagle2 phasing chr${CHR}"
     if [ `bcftools view ${OP}.input.bcf ${CHR} | grep -m 1 "^#CHROM" -A 1 | wc -l` -eq 2 ]
